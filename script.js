6a6ebbf1a8ed48d210a366ef811b3b4951e1b71e
dani morillo <danifromecuador@gmail.com>
2023-03-22 01:23:22 -0500

    Create localStorage functionality

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const addedBooks = document.querySelector('.added-books'); // In this div the html wil be created dinamically
-const books = []; // In this array all the new books will be added
+let books = []; // In this array all the new books will be added
 let removeButton = [];
+
+// add new book to books array and delete input fields for new input
 function add() {
   const newTitle = document.querySelector('.add-title'); // User input
   const newAuthor = document.querySelector('.add-author'); // User input
@@ -9,15 +11,19 @@ function add() {
       title: newTitle.value,
       author: newAuthor.value,
     });
+    localStorage.setItem('storedBooks', JSON.stringify(books));
     newTitle.value = '';
     newAuthor.value = '';
   }
 }
 
+// remove a book from books array
 function remove(index) {
   books.splice(index, 1);
+  localStorage.setItem('storedBooks', JSON.stringify(books));
 }
 
+// print on the HTML if a book is added or removed
 function print() {
   addedBooks.innerHTML = '';
   for (let i = 0; i < books.length; i += 1) {
@@ -34,6 +40,8 @@ function print() {
     `;
     addedBooks.innerHTML += html;
   }
+
+  // before using removeButton we need to create it with this: addedBooks.innerHTML += html;
   removeButton = document.querySelectorAll('.remove-number');
   for (let i = 0; i < books.length; i += 1) {
     removeButton[i].addEventListener('click', () => {
@@ -42,6 +50,14 @@ function print() {
     });
   }
 }
+
+// when the window loads the local storage will be printed
+window.addEventListener('load', () => {
+  if (localStorage.getItem('storedBooks')) {
+    books = JSON.parse(localStorage.getItem('storedBooks'));
+  }
+  print();
+});
 
 const addButton = document.querySelector('.add-button');
 addButton.addEventListener('click', () => {
