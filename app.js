385b20eee18d20d23311b7ae4d9b4959e0223988
dani morillo <danifromecuador@gmail.com>
2023-03-23 18:33:54 -0500

    Change html css and js

--- app.js
+++ app.js
@@ -1,76 +1,74 @@
-class Books {
-  constructor() {
-    this.books = JSON.parse(localStorage.getItem('books')) || [];
+// Book is the class
+// book is the instance of Book class
+// booksArray is the array that contains all book objects
+const addedBooks = document.querySelector('.added-books'); // In this div the html wil be created dinamically
+let booksArray = []; // In this array all the new books will be added
+let removeButtonArray = []; // It will contain all the remove buttons
+const addButton = document.querySelector('.add-button');
+
+window.addEventListener('load', () => {
+  if (localStorage.getItem('added-books')) {
+    booksArray = JSON.parse(localStorage.getItem('added-books'));   
+    Book.render();
+  }
+});
+
+// the Book class contains: constructor, add, remove, and print methods 
+class Book {
+  constructor(title, author) {
+    this.title = title;
+    this.author = author;
   }
 
-  displayBooks() {
-    const booksList = document.getElementById('books-list');
-    booksList.innerHTML = '';
-    this.books.forEach((book) => {
-      const li = document.createElement('li');
-      li.classList.add('book');
-      li.innerHTML = `<div>"${book.title}" by ${book.author}</div>`;
-      const removeButton = document.createElement('button');
-      removeButton.innerHTML = 'Remove';
-      removeButton.addEventListener('click', () => {
-        this.removeBook(book);
+  static addBook() {
+    const newTitle = document.querySelector('.add-title'); // User input
+    const newAuthor = document.querySelector('.add-author'); // User input
+    if (newTitle.value !== '' && newAuthor.value !== '') {
+      let book = new Book(newTitle.value, newAuthor.value);
+      booksArray.push(book);
+      localStorage.setItem('added-books', JSON.stringify(booksArray));
+      newTitle.value = '';
+      newAuthor.value = '';
+    }
+  }
+
+  static removeBook(index) {
+    booksArray.splice(index, 1);
+    localStorage.setItem('added-books', JSON.stringify(booksArray));
+  }
+
+  static render() {
+    addedBooks.innerHTML = '';
+    for (let i = 0; i < booksArray.length; i += 1) {
+      const html = `
+        <div class="book">
+          <div class="book-details">
+            <div class="title">${booksArray[i].title}</div>
+            <div class="author">${booksArray[i].author}</div>
+          </div>
+          <div class="remove-container">
+            <button class="remove-book">Remove</button>
+          </div>
+        </div>
+      `;
+      addedBooks.innerHTML += html;
+    }
+
+    // I can't select the remove button before creating one
+    // A new remove button is created only after click on Add button
+    // A new remove button array is created only after the page renders
+    removeButtonArray = document.querySelectorAll('.remove-book');
+    for (let i = 0; i < removeButtonArray.length; i += 1) {
+      removeButtonArray[i].addEventListener('click', () => {
+        Book.removeBook(i);
+        Book.render();
       });
-      li.appendChild(removeButton);
-      booksList.appendChild(li);
-      li.style.listStyleType = 'none';
-      const hr = document.createElement('hr');
-      booksList.appendChild(hr);
-    });
-  }
-
-  addBook(title, author) {
-    const book = { title, author };
-    this.books.push(book);
-    localStorage.setItem('books', JSON.stringify(this.books));
-    this.displayBooks();
-  }
-
-  removeBook(book) {
-    const newBooks = this.books.filter((b) => b.title !== book.title || b.author !== book.author);
-    localStorage.setItem('books', JSON.stringify(newBooks));
-    this.books = newBooks;
-    this.displayBooks();
+    }
   }
 }
 
-const alertMessage = document.querySelector('.alert-message');
-document.addEventListener('DOMContentLoaded', () => {
-  const books = new Books();
-  books.displayBooks();
-  const form = document.getElementById('add-book-form');
-  form.addEventListener('submit', (event) => {
-    event.preventDefault();
-    const title = document.getElementById('title').value;
-    const author = document.getElementById('author').value;
-
-    // check if the book is already in the list
-    const booksArrays = JSON.parse(localStorage.getItem('books')) || [];
-    let bookExists = false;
-    if (booksArrays.length > 0) {
-      for (let i = 0; i < booksArrays.length; i += 1) {
-        if (booksArrays[i].title === title && booksArrays[i].author === author) {
-          bookExists = true;
-        }
-        if (bookExists) alertMessage.innerHTML = `The book ${title} by ${author} already exists`;
-      }
-    }
-    if (bookExists === false) {
-      books.addBook(title, author);
-    }
-    // end of the check
-
-    form.reset();
-  });
+addButton.addEventListener('click', () => {
+  Book.addBook();
+  Book.render();  
+  removeButtonArray = document.querySelectorAll('.remove-book');  
 });
-
-const titleInput = document.querySelector('#title');
-titleInput.addEventListener('click', () => {
-  alertMessage.innerHTML = '';
-});
-
-//let's create something amazing!
