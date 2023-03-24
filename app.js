68c3617e31097f9507090cfb216c2c81adf505d5
dani morillo <danifromecuador@gmail.com>
2023-03-23 20:48:53 -0500

    Add navigation using Single Page Apps

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ class Book {
       const html = `
         <div class="book">
           <div class="book-details">
-            <div class="title">${booksArray[i].title}</div>
+            <div class="title">"${booksArray[i].title}" by</div>
             <div class="author">${booksArray[i].author}</div>
           </div>
           <div class="remove-container">
@@ -71,4 +71,31 @@ addButton.addEventListener('click', () => {
   Book.addBook();
   Book.render();
   removeButtonArray = document.querySelectorAll('.remove-book');
+});
+
+// 
+const navList = document.querySelector('.nav-list');
+const navAdd = document.querySelector('.nav-add');
+const navContact = document.querySelector('.nav-contact');
+const listSection = document.querySelector('.list');
+const addANewSection = document.querySelector('.add-a-new-book') ;
+const contactSection = document.querySelector('.contact');
+
+
+navList.addEventListener('click', () => {
+  listSection.classList.remove('hide');
+  addANewSection.classList.add('hide');
+  contactSection.classList.add('hide');
+});
+
+navAdd.addEventListener('click', () => {
+  listSection.classList.add('hide');
+  addANewSection.classList.remove('hide');
+  contactSection.classList.add('hide');
+});
+
+navContact.addEventListener('click', () => {
+  listSection.classList.add('hide');
+  addANewSection.classList.add('hide');
+  contactSection.classList.remove('hide');
 });
