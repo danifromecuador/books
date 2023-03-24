6b2f12fbe548886c15e8419ef36885eb17d1cb15
dani morillo <danifromecuador@gmail.com>
2023-03-23 20:50:59 -0500

    Fix linter errors

--- app.js
+++ app.js
@@ -73,14 +73,13 @@ addButton.addEventListener('click', () => {
   removeButtonArray = document.querySelectorAll('.remove-book');
 });
 
-// 
+//
 const navList = document.querySelector('.nav-list');
 const navAdd = document.querySelector('.nav-add');
 const navContact = document.querySelector('.nav-contact');
 const listSection = document.querySelector('.list');
-const addANewSection = document.querySelector('.add-a-new-book') ;
+const addANewSection = document.querySelector('.add-a-new-book');
 const contactSection = document.querySelector('.contact');
-
 
 navList.addEventListener('click', () => {
   listSection.classList.remove('hide');
